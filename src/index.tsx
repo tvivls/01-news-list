4aba9573d847f84dd63f8e7fa5874e63afcf935d
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-13 21:10:50 +0300

    fix: (index.tsx): убран реакт

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,8 +5,8 @@ import { Global } from './styles/newsStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.Fragment>
+  <>
     <Global />
     <App />
-  </React.Fragment>,
+  </>,
 );
