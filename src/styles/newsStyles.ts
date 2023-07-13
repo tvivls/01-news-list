98207d5d1a218a50e2b3cb03a3b2d21e37628964
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-13 21:00:49 +0300

    feat (newsStyles): добавлены некоторые стили

---
+++ src/styles/newsStyles.ts
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const NewsCardStyles = styled.div`
+  border: none;
+  padding: 0 30px;
+`;
+
+export const Global = createGlobalStyle`
+body {
+  background: #e8f4fd;
+}`;
