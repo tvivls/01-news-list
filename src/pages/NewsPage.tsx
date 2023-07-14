4ae9393c319a816266c80e077b08d97889403e38
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-15 01:39:55 +0300

    fix (NewsPage): ошибки вынесены в компонент StateError

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import News from '../components/News';
 import newsStore from '../store/newsStore';
-import { Alert, AlertTitle, CircularProgress } from '@mui/material';
 import { observer } from 'mobx-react-lite';
+import StateError from '../components/StateError';
 
 const NewsPage = observer(() => {
   const { getNewsAction, newsId } = newsStore;
@@ -15,17 +15,10 @@ const NewsPage = observer(() => {
     return () => clearInterval(intervalId);
   }, []);
 
-  if (newsId?.state === 'pending') return <CircularProgress sx={{ position: 'absolute', top: '50%', left: '50%' }} />;
-  if (newsId?.state === 'rejected')
-    return (
-      <Alert severity="error">
-        <AlertTitle>Error</AlertTitle>
-        Rejected
-      </Alert>
-    );
-
-  return newsId?.value ? (
-    <>{newsId?.value?.map((storyId: number) => <News key={storyId && storyId} id={storyId && storyId} />)}</>
+  return newsId?.state === 'pending' || newsId?.state === 'rejected' ? (
+    <StateError state={newsId?.state} />
+  ) : newsId?.value ? (
+    <>{newsId?.value?.map((storyId: number) => <News key={storyId} id={storyId} />)}</>
   ) : null;
 });
 
