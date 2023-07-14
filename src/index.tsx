229c1288151acaf8120a1a91933d67053cb5809d
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-15 01:46:17 +0300

    fix (index.css index.tsx): исправлены глобальные стили

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Global } from './styles/newsStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <>
-    <Global />
     <App />
   </>,
 );
