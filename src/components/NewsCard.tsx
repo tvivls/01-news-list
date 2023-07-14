4823cc7727e015465395d21c5b47507b254813d9
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-15 01:37:33 +0300

    fix (NewsCard): вынесены вычисления и исправлены теги

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -2,40 +2,40 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import List from '@mui/material/List';
 import { NewsType } from '../utils/types';
-import { Link, Rating, Typography } from '@mui/material';
-import { NewsCardStyles } from '../styles/newsStyles';
+import { Rating, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { STORY_ROUTE } from '../utils/consts';
+import { StyledLink } from '../styles/newsStyles';
 
 const NewsCard = ({ title, score, by, time, id }: NewsType) => {
   const navigate = useNavigate();
+  let date = '';
 
   const handleNavigate = () => {
     navigate(`${STORY_ROUTE}/${id}`);
   };
 
+  if (time) date = new Date(1000 * time).toUTCString();
+
   return (
-    <NewsCardStyles>
-      <Link onClick={handleNavigate} sx={{ textDecoration: 'none', color: 'black', padding: '0 1px' }}>
-        <List
-          sx={{
-            bgcolor: 'background.paper',
-            padding: '0',
-            borderRadius: '10px',
-          }}
-        >
-          <Typography color="text.secondary" display="block" variant="caption" sx={{ padding: '0 18px' }}>
-            {by}
-          </Typography>
-          <ListItem sx={{ padding: '0 18px' }}>
-            <ListItemText primary={title} secondary={time && new Date(1000 * time).toUTCString()} />
-          </ListItem>
-          <div>
-            <Rating name="read-only" value={score} readOnly sx={{ padding: '0 15px' }} />
-          </div>
-        </List>
-      </Link>
-    </NewsCardStyles>
+    <StyledLink onClick={handleNavigate}>
+      <List
+        sx={{
+          bgcolor: 'background.paper',
+          padding: '0',
+          margin: '0 30px',
+          borderRadius: '10px',
+        }}
+      >
+        <Typography color="text.secondary" display="block" variant="caption" sx={{ padding: '0 18px' }}>
+          {by}
+        </Typography>
+        <ListItem sx={{ padding: '0 18px' }}>
+          <ListItemText primary={title} secondary={date} />
+        </ListItem>
+        <Rating name="read-only" value={score} readOnly sx={{ padding: '0 15px' }} />
+      </List>
+    </StyledLink>
   );
 };
 
