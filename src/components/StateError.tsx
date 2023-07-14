30fef9209888347444bddef82de7b7b4dd2f9a5d
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-15 01:26:13 +0300

    feat (StateError): добавлен компонент для ошибок

---
+++ src/components/StateError.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Alert, AlertTitle, CircularProgress } from '@mui/material';
+import { StyledBox } from '../styles/newsStyles';
+
+type ErrorType = { state: 'pending' | 'rejected' };
+
+const StateError = ({ state }: ErrorType) => {
+  return state === 'pending' ? (
+    <StyledBox>
+      <CircularProgress />
+    </StyledBox>
+  ) : (
+    <Alert severity="error">
+      <AlertTitle>Error</AlertTitle>
+      Rejected
+    </Alert>
+  );
+};
+
+export default StateError;
