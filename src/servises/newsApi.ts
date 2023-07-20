7a67fd92d263b22f3ff877a5f77a292de79d3d86
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-21 00:05:47 +0300

    fix (newsApi): исправлены типы

--- src/servises/newsApi.ts
+++ src/servises/newsApi.ts
@@ -1,14 +1,13 @@
 import get from 'axios';
-import { NewsType } from '../utils/types';
 
 const baseURL = 'https://hacker-news.firebaseio.com/v0/';
 const newStoriesURL = `${baseURL}newstories.json`;
-const storyURL = `${baseURL}item/`;
+export const storyURL = `${baseURL}item/`;
 
 export const getStoriesId = async (): Promise<number[]> => {
   return await get(newStoriesURL).then(({ data }) => data && data.slice(0, 100));
 };
 
-export const getStory = async (storyId: number): Promise<NewsType> => {
+export const getStory = async <T>(storyId: number): Promise<T> => {
   return await get(`${storyURL + storyId}.json`).then(({ data }) => data && data);
 };
